3289192ec99c89f5506c5a2595f1c8561f01ef61
xrotwang <xrotwang@googlemail.com>
2016-07-15 15:32:37 +0200

    integrated more datasets

---
+++ datasets/akse/__init__.py
@@ -0,0 +1,53 @@
+# coding: utf8
+from __future__ import unicode_literals, print_function, division
+
+from clldutils.dsv import UnicodeReader
+from clldutils.misc import slug
+
+from pylexibank.util import xls2csv
+from pylexibank.dataset import CldfDataset
+
+
+def download(dataset):
+    xls2csv(dataset.raw.joinpath('Semitic.Wordlists.xls'), outdir=dataset.raw)
+
+
+def cldf(dataset, glottolog, concepticon, **kw):
+    concepticon = {
+        c['ENGLISH']: c['CONCEPTICON_ID']
+        for c in concepticon.conceptlist(dataset.conceptlist)}
+
+    language_map = {l['NAME']: l['GLOTTOCODE'] or None for l in dataset.languages}
+
+    header, rows = None, []
+    with UnicodeReader(dataset.raw.joinpath('Semitic.Wordlists.ActualWordlists.csv')) as reader:
+        for i, row in enumerate(reader):
+            row = [c.strip() for c in row]
+            if i == 0:
+                header = row
+            if i > 0:
+                rows.append(row)
+
+    langs = header[1:]
+    with CldfDataset((
+            'ID',
+            'Language_ID',
+            'Language_name',
+            'Parameter_ID',
+            'Parameter_name',
+            'Value',
+            ), dataset) as ds:
+        for row in rows:
+            concept = row[0]
+            for i, col in enumerate(row[1:]):
+                if not col or col == '---':
+                    continue
+                lang = langs[i]
+                ds.add_row([
+                    '%s-%s' % (slug(lang), slug(concept)),
+                    language_map[lang],
+                    lang,
+                    concepticon[concept],
+                    concept,
+                    col,
+                ])
