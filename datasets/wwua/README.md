d7c9f409cc4ccf5b34d9a4ad9f444389b1583c04
xrotwang <xrotwang@googlemail.com>
2016-07-15 17:13:28 +0200

    added wwua

---
+++ datasets/wwua/README.md
@@ -0,0 +1,17 @@
+## The evolution and biogeography of Uto-Aztecan languages - supporting information
+
+Cite the source dataset as
+
+> Wheeler and Whiteley 2015. Historical linguistics as a sequence optimization problem: the evolution and biogeography of Uto-Aztecan languages
+
+Available online at http://onlinelibrary.wiley.com/doi/10.1111/cla.12078/suppinfo
+
+### Cognate sets
+0 cognates in 0 cognate sets
+
+### Lexemes
+
+Name | Languages | Concepts | Lexemes | Quality
+:--- | ---:| ---:| ---:|:---:
+**total:** | 34 | 101 | 5,035 | 
+[wwua](cldf/wwua.csv) | 34 | 101 | 5035 | ![Glottolog: 83%](https://img.shields.io/badge/Glottolog-83%25-yellowgreen.svg "Glottolog: 83%") ![Concepticon: 100%](https://img.shields.io/badge/Concepticon-100%25-brightgreen.svg "Concepticon: 100%") ![Source: 0%](https://img.shields.io/badge/Source-0%25-red.svg "Source: 0%")
---
+++ datasets/wwua/__init__.py
@@ -0,0 +1,66 @@
+# coding: utf8
+from __future__ import unicode_literals, print_function, division
+
+from six.moves.urllib.request import urlretrieve
+from clldutils.dsv import UnicodeReader
+from clldutils.misc import slug
+
+from pylexibank.util import with_temp_dir, xls2csv
+from pylexibank.dataset import CldfDataset
+
+
+URL = "http://onlinelibrary.wiley.com/store/10.1111/cla.12078/asset/supinfo/cla12078-sup-0006-supinfo6.xls?v=1&s=4c895c80efa148e7872c3c7702c5df3ed192c236"
+FILENAME = "cla12078-sup-0006-supinfo6.xls"
+
+
+def download(dataset):
+    with with_temp_dir() as tmpdir:
+        urlretrieve(URL, tmpdir.joinpath(FILENAME).as_posix())
+        xls2csv(tmpdir.joinpath(FILENAME), outdir=dataset.raw)
+
+
+def iterforms(s):
+    for i, form in enumerate(s.split('|')):
+        yield i + 1, form.strip()
+
+
+def cldf(dataset, glottolog, concepticon, **kw):
+    concepticon = {
+        c['ENGLISH']: c['CONCEPTICON_ID']
+        for c in concepticon.conceptlist(dataset.conceptlist)}
+
+    language_map = {l['NAME']: l['GLOTTOCODE'] or None for l in dataset.languages}
+
+    with UnicodeReader(dataset.raw.joinpath(FILENAME.replace('xls', 'Sheet1.csv'))) as r:
+        rows = [row for row in r]
+
+    concepts = [
+        (i, rows[0][i].replace('_', ' ').strip()) for i in range(1, len(rows[0]), 2)]
+    assert all(concept in concepticon for _, concept in concepts)
+
+    with CldfDataset((
+            'ID',
+            'Language_ID',
+            'Language_name',
+            'Parameter_ID',
+            'Parameter_name',
+            'Value',
+            ), dataset) as ds:
+        ds.table.schema.columns['Value']['dc:format'] = 'IPA'
+
+        for row in rows[3:]:
+            row = [col.strip() for col in row]
+            if not row[0]:
+                continue
+            lname = row[0]
+            for i, concept in concepts:
+                for j, form in iterforms(row[i]):
+                    if form != '?':
+                        ds.add_row([
+                            '%s-%s-%s' % (slug(lname), (i + 1) // 2, j),
+                            language_map[lname],
+                            lname.replace('_', ' '),
+                            concepticon[concept],
+                            concept,
+                            form,
+                        ])
